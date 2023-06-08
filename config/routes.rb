7eaed572e3752bfc92837edb23a591be782a1f0d
tomo178 <inaba.dangan@gmail.com>
2023-06-08 15:00:03 +0000

    Add a full_title helper

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
+  root "static_pages#home"
   get 'static_pages/home'
   get 'static_pages/help'
   get  "static_pages/about"
   get 'microposts/new'
-  root "application#hello"
+  # root "application#hello"
   resources :microposts, only: [:new, :create]
 end
