8023d155645816357f2b1daefd6b2dbd4b34e312
tomo178 <inaba.dangan@gmail.com>
2023-06-08 11:35:50 +0000

    Finish static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  get 'static_pages/home'
+  get 'static_pages/help'
+  get  "static_pages/about"
+  get 'microposts/new'
   root "application#hello"
-  # Defines the root path route ("/")
-  # root "articles#index"
+  resources :microposts, only: [:new, :create]
 end
